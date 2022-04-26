0360f07fff2a0201143ef5c2fc044b8a1a167584
Evgeniy Parfenov <epparfenov@yandex-team.ru>
2022-04-26 14:38:25 +0300

    readme add

--- ps-init/adds/init.ps1
+++ ps-init/adds/init.ps1
@@ -29,87 +29,42 @@ Start-BitsTransfer -Source `"https://raw.githubusercontent.com/Sayanaro/YandexCl
 Start-BitsTransfer -Source `"https://raw.githubusercontent.com/Sayanaro/YandexCloud-Prcticum-Security-Course/master/data/SubCA.crl`" -Destination `"C:\Stages`"
 Start-BitsTransfer -Source `"https://raw.githubusercontent.com/Sayanaro/YandexCloud-Prcticum-Security-Course/master/data/SubCA+.crl`" -Destination `"C:\Stages`"
 
-`$outScript = `"powershell.exe -ExecutionPolicy Bypass -File C:\Stages\StageService3.ps1`"
-`$outScript | Out-File -FilePath `"C:\Stages\StageService3.bat`" -Encoding ascii -Confirm:`$false -Force
+#`$outScript = `"powershell.exe -ExecutionPolicy Bypass -File C:\Stages\StageService3.ps1`"
+#`$outScript | Out-File -FilePath `"C:\Stages\StageService3.bat`" -Encoding ascii -Confirm:`$false -Force
 
-`$outScript = `"powershell.exe -ExecutionPolicy Bypass -File C:\Stages\StageService2.ps1`"
-`$outScript | Out-File -FilePath `"C:\Stages\StageService2.bat`" -Encoding ascii -Confirm:`$false -Force
+#`$outScript = `"powershell.exe -ExecutionPolicy Bypass -File C:\Stages\StageService2.ps1`"
+#`$outScript | Out-File -FilePath `"C:\Stages\StageService2.bat`" -Encoding ascii -Confirm:`$false -Force
 
-`$IPAddress = Get-NetIPAddress -AddressFamily IPv4 | where {`$_.InterfaceAlias -notlike `"*Loopback*`"}
-`$Gateway = (Get-NetIPConfiguration).IPv4DefaultGateway
-`$outNull = Remove-NetIPAddress -InterfaceIndex `$IPAddress.InterfaceIndex -Confirm:`$false
-`$outNull = New-NetIPAddress -IPAddress `$IPAddress.IPAddress -DefaultGateway `$Gateway.NextHop -PrefixLength `$IPAddress.PrefixLength -InterfaceIndex `$IPAddress.InterfaceIndex
+#`$IPAddress = Get-NetIPAddress -AddressFamily IPv4 | where {`$_.InterfaceAlias -notlike `"*Loopback*`"}
+#`$Gateway = (Get-NetIPConfiguration).IPv4DefaultGateway
+#`$outNull = Remove-NetIPAddress -InterfaceIndex `$IPAddress.InterfaceIndex -Confirm:`$false
+#`$outNull = New-NetIPAddress -IPAddress `$IPAddress.IPAddress -DefaultGateway `$Gateway.NextHop -PrefixLength `$IPAddress.PrefixLength -InterfaceIndex `$IPAddress.InterfaceIndex
 `$outNull = Set-DNSClientServerAddress -InterfaceIndex `$IPAddress.InterfaceIndex -ServerAddresses `"127.0.0.1`",`$IPAddress.IPAddress
 
-`$RegArgLoadSystemHive = `"HKLM\System`"
-`$RegServicesPath = `"HKLM:\System\ControlSet001\Services\`"
-`$RegStageService = `$RegServicesPath + `"StageService\`"
+`$RegStageService = `"HKLM\System\ControlSet001\Services\StageService\`"
 `$RegStageServiceEnum = `$RegStageService + `"Enum\`"
-`$VHDSystemHive = `"C:\WINDOWS\SYSTEM32\CONFIG\SYSTEM`"
 `$RegStageServiceParameters = `$RegStageService + `"Parameters\`"
 `$RegStageServiceSecurity = `$RegStageService + `"Security\`"
 
-
-. reg load `"`$RegArgLoadSystemHive`" `$VHDSystemHive
-Start-Sleep -Seconds 5
-
-`$supress = New-Item -Path `$RegServicesPath -Name StageService
-[gc]::collect()
-Start-Sleep -Seconds 5
-`$supress = New-Item -Path $`RegStageService -Name Enum
-[gc]::collect()
-Start-Sleep -Seconds 5
-`$supress = New-Item -Path `$RegStageService -Name Parameters
-[gc]::collect()
-Start-Sleep -Seconds 5
-`$supress = New-Item -Path `$RegStageService -Name Security
-[gc]::collect()
-Start-Sleep -Seconds 5
-`$supress = New-ItemProperty -Path `$RegStageService -Name Type -PropertyType Dword -Value 16
-[gc]::collect()
-Start-Sleep -Seconds 5
-`$supress = New-ItemProperty -Path `$RegStageService -Name Start -PropertyType Dword -Value 2
-[gc]::collect()
-Start-Sleep -Seconds 5
-`$supress = New-ItemProperty -Path `$RegStageService -Name DelayedAutostart -PropertyType Dword -Value 1
-[gc]::collect()
-Start-Sleep -Seconds 5
-`$supress = New-ItemProperty -Path `$RegStageService -Name ErrorControl -PropertyType Dword -Value 1
-[gc]::collect()
-Start-Sleep -Seconds 5
-`$supress = New-ItemProperty -Path `$RegStageService -Name WOW64 -PropertyType Dword -Value 1
-[gc]::collect()
-Start-Sleep -Seconds 5
-`$supress = New-ItemProperty -Path `$RegStageService -Name DisplayName -PropertyType String -Value 'StageService'
-[gc]::collect()
-Start-Sleep -Seconds 5
-`$supress = New-ItemProperty -Path `$RegStageService -Name ObjectName -PropertyType String -Value 'LocalSystem'
-[gc]::collect()
-Start-Sleep -Seconds 5
-`$supress = New-ItemProperty -Path `$RegStageService -Name ImagePath -PropertyType ExpandString -Value 'C:\Stages\srvany.exe'
-[gc]::collect()
-Start-Sleep -Seconds 5
-`$supress = New-ItemProperty -Path `$RegStageServiceEnum -Name '0' -PropertyType String -Value 'Root\LEGACY_StageService\0000'
-[gc]::collect()
-Start-Sleep -Seconds 5
-`$supress = New-ItemProperty -Path `$RegStageServiceEnum -Name Count -PropertyType Dword -Value 1
-[gc]::collect()
-Start-Sleep -Seconds 5
-`$supress = New-ItemProperty -Path `$RegStageServiceEnum -Name NextInstance -PropertyType Dword -Value 1
-[gc]::collect()
-Start-Sleep -Seconds 5
-`$supress = New-ItemProperty -Path `$RegStageServiceParameters -Name Application -PropertyType String -Value 'C:\Stages\StageService2.bat'
-[gc]::collect()
-Start-Sleep -Seconds 5
-`$supress = New-ItemProperty -Path `$RegStageServiceSecurity -Name Security -PropertyType Binary -Value ([byte[]](0x01,0x00,0x14,0x80,0xb8,0x00,0x00,0x00,0xc4,0x00,0x00,0x00,0x14,0x00,0x00,0x00,0x30,0x00,0x00,0x00,0x02,0x00,0x1c,0x00,0x01,0x00,0x00,0x00,0x02,0x80,0x14,0x00,0xff,0x01,0x0f,0x00,0x01,0x01,0x00,0x00,0x00,0x00,0x00,0x01,0x00,0x00,0x00,0x00,0x02,0x00,0x88,0x00,0x06,0x00,0x00,0x00,0x00,0x00,0x14,0x00,0xfd,0x01,0x02,0x00,0x01,0x01,0x00,0x00,0x00,0x00,0x00,0x05,0x12,0x00,0x00,0x00,0x00,0x00,0x18,0x00,0xff,0x01,0x0f,0x00,0x01,0x02,0x00,0x00,0x00,0x00,0x00,0x05,0x20,0x00,0x00,0x00,0x20,0x02,0x00,0x00,0x00,0x00,0x14,0x00,0x8d,0x01,0x02,0x00,0x01,0x01,0x00,0x00,0x00,0x00,0x00,0x05,0x04,0x00,0x00,0x00,0x00,0x00,0x14,0x00,0x8d,0x01,0x02,0x00,0x01,0x01,0x00,0x00,0x00,0x00,0x00,0x05,0x06,0x00,0x00,0x00,0x00,0x00,0x14,0x00,0x00,0x01,0x00,0x00,0x01,0x01,0x00,0x00,0x00,0x00,0x00,0x05,0x0b,0x00,0x00,0x00,0x00,0x00,0x18,0x00,0xfd,0x01,0x02,0x00,0x01,0x02,0x00,0x00,0x00,0x00,0x00,0x05,0x20,0x00,0x00,0x00,0x23,0x02,0x00,0x00,0x01,0x01,0x00,0x00,0x00,0x00,0x00,0x05,0x12,0x00,0x00,0x00,0x01,0x01,0x00,0x00,0x00,0x00,0x00,0x05,0x12,0x00,0x00,0x00))
-[gc]::collect()
-Start-Sleep -Seconds 5
-`$supress = Set-ItemProperty -Path `"`$(`$RegServicesPath)ParPort`" -Name Start -Value 4
-[gc]::collect()
-Start-Sleep -Seconds 5
-[gc]::collect()
-
-. reg unload `$RegArgLoadSystemHive
+reg add `$RegStageService
+reg add `$RegStageServiceEnum
+reg add `$RegStageServiceParameters
+reg add `$RegStageServiceSecurity
+reg add `$RegStageService /v Type /t REG_DWORD /d 16
+reg add `$RegStageService /v Start /t REG_DWORD /d 2
+#reg add `$RegStageService /v DelayedAutostart /t REG_DWORD /d 1
+reg add `$RegStageService /v ErrorControl /t REG_DWORD /d 1
+reg add `$RegStageService /v WOW64 /t REG_DWORD /d 1
+reg add `$RegStageService /v DisplayName /t REG_SZ /d StageService
+reg add `$RegStageService /v ObjectName /t REG_SZ /d LocalSystem
+reg add `$RegStageService /v ImagePath /t REG_EXPAND_SZ /d 'C:\Stages\srvany.exe'
+reg add `$RegStageServiceEnum /v 0 /t REG_SZ /d 'Root\LEGACY_StageService\0000'
+reg add `$RegStageServiceEnum /v Count /t REG_DWORD /d 1
+reg add `$RegStageServiceEnum /v NextInstance /t REG_DWORD /d 1
+reg add `$RegStageServiceParameters /v Application /t REG_SZ /d 'C:\Stages\StageService2.bat'
+reg add `$RegStageServiceSecurity /v Security /t REG_BINARY /d 01001480b8000000c4000000140000003000000002001c000100000002801400ff010f00010100000000000100000000020088000600000000001400fd01020001010000000000051200000000001800ff010f0001020000000000052000000020020000000014008d010200010100000000000504000000000014008d010200010100000000000506000000000014000001000001010000000000050b00000000001800fd01020001020000000000052000000023020000010100000000000512000000010100000000000512000000
+reg delete HKLM\System\ControlSet001\Services\ParPort\ /v Start /f
+reg add HKLM\System\ControlSet001\Services\ParPort\ /v Start /t REG_DWORD /d 4
 
 `$outNull = Install-WindowsFeature AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools -Confirm:`$false
 `$DSRM = ConvertTo-SecureString `"`$DSRMPassword`" -AsPlainText -Force
